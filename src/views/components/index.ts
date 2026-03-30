9a90c20ba9c3b21cce4da6cc0bca410d126a82c3
chungvh <chungvh@brain-net.co.jp>
2026-03-30 14:38:33 +0700

    Dọn dẹp files duplicate

--- src/views/components/index.ts
+++ src/views/components/index.ts
@@ -1,5 +1,4 @@
 export { default as Header } from './Header';
-export { default as ProtectedRoute } from './ProtectedRoute';
 export { default as ProductSelection } from './ProductSelection';
 export { default as CustomizationOptions } from './CustomizationOptions';
 export { default as OrderSummary } from './OrderSummary';
@@ -9,5 +8,3 @@ export { default as Dashboard } from './Dashboard';
 export { default as PendingOrdersPanel } from './PendingOrdersPanel';
 export { default as MergedOrdersHistory } from './MergedOrdersHistory';
 export { default as OrderSummaryReport } from './OrderSummaryReport';
-export { default as ProductSelectionPage } from '../pages/ProductSelection';
-
